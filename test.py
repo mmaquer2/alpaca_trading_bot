46b0f235f676f31c0980727ec6a3f94ea655f491
mmaquer2 <mike.maquera212@gmail.com>
2020-06-16 16:19:42 -0500

    started time series algo

--- test.py
+++ test.py
@@ -1,46 +1,53 @@
 
 ##need to add request package to the file
-import request,json
+##import request,json
 from config import *
+import alpaca_trade_api as tradeapi
 
 
 ##TO DO's and Things to learn:
-## 
+    ##how does self work in a class and the scope of inheritance
 
 
-##URL
-baseUrl = "alpaca.com/moneytrader"
-accountUrl = ''
-secret = secretKey
-clientId = clientId
-searchUrl = baseUrl + secret + clientId
+##URL 
+secKey = alpacaaSecKey
+clientId = alpacaApiKey
 
-##get our account
+class testBot(secKey,clientId,symbol,qty,side,time_in_force,stop_price,limit_price):
+    def __init__(self):
+        this.baseUrl = baseUrl
+        this.secKey = secKey
+        this.clientId = clientId
 
-def get_Acnt():
-    r = requests.get(searchUrl)
+    def getAccount(self):
 
+        api = tradeapi.REST(alpacaApiKey, secKey, api_version='v2') # or use ENV Vars shown below
+        account = api.get_account()
+        api.list_positions()
 
 ## create an order with the account
 
-def createOrder(symbol,qty,side,time_in_force):
-    data = {
-        symbol: 'symbol',
-        qty: 'qty',
-        side:'side',
-        time_in_force:'time_in_force'
-    }
-
-    r = requests.post(data, json= data)
-    return json.loads(r.content)
-
-
-
-
+    def createOrder(symbol,qty,side,time_in_force):
+    api.submit_order(
+        symbol='symbol',
+        side='qty',
+        type='market',
+        qty='100',
+        time_in_force='day',
+        order_class='bracket',
+        take_profit=dict(
+        limit_price='305.0',
+        ),
+        stop_loss=dict(
+            stop_price='295.5',
+            limit_price='295.5',
+        )
+    )
 ##create a paper trade order
+## secKey,clientId,symbol,qty,side,time_in_force,stop_price,limit_price  
+# this is the total args
 
-
-res = createOrder('MSFT','100','buy','gtc')
+res = testBot(secKey,clientId,'MSFT','100','buy','gtc','320','380')
 
 print(res)
 
---
+++ timeSeri.py
@@ -0,0 +1,28 @@
+from config import *
+
+## time series analysis
+
+## inputs:
+##entry signal, time frequency, size, exit, evaluation criteria
+## stock watch list
+## number of shares to buy
+
+## outputs:
+## number of buy signals
+## number of sell signals
+## total price of assets
+##list of stocks purchased
+
+
+class timeSeri(watchList,entrySig,exitSig,timeFreq):
+    def __init__(self):
+        watchList.self = watchList
+        watchList.self = watchList
+        watchList.self = watchList
+        watchList.self = watchList
+
+    def logAcnt():
+
+
+    
+    
